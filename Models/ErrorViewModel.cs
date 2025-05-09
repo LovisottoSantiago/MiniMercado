e988d7563169de5774b22cc8b62e8355f6eca58e
LovisottoSantiago <santiagolovisotto@gmail.com>
2025-05-09 15:44:45 -0300

    Creacion del repositorio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiniMercado.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
