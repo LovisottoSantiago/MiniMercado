7f9871c8d2a19d3fc6f5bf238a7722c14c852060
LovisottoSantiago <santiagolovisotto@gmail.com>
2025-05-19 13:13:33 -0300

    Ventas creen

---
+++ Controllers/VentaScreenController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiniMercado.Data;
+using MiniMercado.Models;
+
+namespace MiniMercado.Controllers
+{
+    public class VentaScreenController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
+
+        public VentaScreenController(ILogger<HomeController> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var productos = _context.Producto
+                .Include(p => p.ProveedorNavigation) // Incluye la relación
+                .ToList();
+            return View(productos);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=MainMenu}/{action=Index}/{id?}");
 
 app.Run();
